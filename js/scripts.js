09d69f829478f477de1ae1c8b6d89b8419fdf834
rinkyn05 <rinkyn2005@gmail.com>
2025-01-27 09:40:13 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+// Por ahora, no se incluye funcionalidad JavaScript adicional.
+// Se puede agregar animaciones o interactividad si lo necesitas en el futuro.
+console.log('Portafolio cargado correctamente.');
